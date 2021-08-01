63ce9321687571cd3b692e0b23cd62a11bb0a0c6
Dor-Yan <dorian.margraf@gmail.com>
2021-08-01 17:54:44 +0200

    Dodana baza danych

---
+++ SquashMVC.Application/Interfaces/IPlayerRepository.cs
@@ -0,0 +1,30 @@
+﻿using SquashMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquashMVC.Application.Interfaces
+{
+    public interface IPlayerRepository
+    {
+        
+        void DeletePlayer(int playerId);
+
+
+        int AddPlayer(Player player);
+
+
+        IQueryable<Player> GetPlayersBySexId(int sexId);
+
+
+        IQueryable<Player> GetPlayersByAssociationId(int associationId);
+
+
+        IQueryable<Player> GetPlayersByCountryId(int countryId);
+
+
+        Player GetPlayerById(int playerID);
+    }
+}
--- SquashMVC.Infrastructure/Repositories/PlayerRepository.cs
+++ SquashMVC.Infrastructure/Repositories/PlayerRepository.cs
@@ -1,4 +1,5 @@
-﻿using SquashMVC.Domain.Model;
+﻿using SquashMVC.Application.Interfaces;
+using SquashMVC.Domain.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace SquashMVC.Infrastructure.Repositories
 {
-    class PlayerRepository
+    class PlayerRepository : IPlayerRepository
     {
         private readonly Context _context;
 
